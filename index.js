fdf7f01710ddcddc5b09388f49607024c593b686
Esau Suarez <esau@turo.com>
2022-12-21 16:01:50 +0000

    fix: exports configuration following recommended settings

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var config = require("@commitlint/config-conventional");
-
-// Increase to 300 characters for max line length in body
-config["rules"]["body-max-line-length"] = [2, "always", 300];
-
-module.exports = config;
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+    // Increase to 300 characters for max line length in body
+        "body-max-line-length": [2, "always", 300],
+    },
+};
