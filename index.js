f872bf08b84ae8a861f5479a52a1435b99e506ed
Esau Suarez <esau@turo.com>
2023-03-27 10:40:29 +0100

    feat: increases max footer length for breaking changes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
     rules: {
-    // Increase to 300 characters for max line length in body
+        // Increase to 300 characters for max line length in body
         "body-max-line-length": [2, "always", 300],
+        // Increase to 300 characters for max line length in footer
+        "footer-max-line-length": [2, "always", 300],
     },
 };
